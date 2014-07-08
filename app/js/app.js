939c7e46d445d037211d767692a3dea547e2f9bd
snelson3 <snelson@opalytics.com>
2014-07-08 10:43:38 -0700

    Basic layout of site finished using bootstrap, need to make it functional using angular

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,12 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/rankings', {templateUrl: 'partials/rankings.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/pr', {templateUrl: 'partials/powerrankings.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/tournaments', {templateUrl: 'partials/tournaments.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/submit', {templateUrl: 'partials/submit.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/tourney', {templateUrl: 'partials/tournament.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/profile', {templateUrl: 'partials/profile.html', controller: 'MyCtrl1'})
+  $routeProvider.otherwise({redirectTo: '/rankings'});
 }]);
