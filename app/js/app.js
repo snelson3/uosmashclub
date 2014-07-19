a1c526b63864ec56341d0d9b3deed7493eb19b7c
snelson3 <snelson@opalytics.com>
2014-07-18 17:51:55 -0700

    Halfway at a solution, but can't translate coffeescript, so I'm going back to an old solution I might have but keeping this just in case

--- app/js/app.js
+++ app/js/app.js
@@ -10,15 +10,24 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/rankings', {templateUrl: 'partials/rankings.html', controller: 'RankingCtrl'});
-  $routeProvider.when('/pr', {templateUrl: 'partials/powerrankings.html', controller: 'PowerRankingCtrl'});
-  $routeProvider.when('/tournaments', {templateUrl: 'partials/tournaments.html', controller: 'TournamentCtrl'});
-  $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'ContactCtrl'});
-  $routeProvider.when('/submit', {templateUrl: 'partials/submit.html', controller: 'DummyCtrl'});
+  $routeProvider.when('/rankings', {templateUrl: 'partials/rankings.html', controller: 'RankingCtrl'
+  ,img: 'falcon', ttl: 'Oregon ELO Rankings'});
+  $routeProvider.when('/pr', {templateUrl: 'partials/powerrankings.html', controller: 'PowerRankingCtrl'
+  ,img: 'fox', ttl: 'Power Rankings'});
+  $routeProvider.when('/tournaments', {templateUrl: 'partials/tournaments.html', controller: 'TournamentCtrl'
+  ,img: 'mango', ttl: 'Tournaments'});
+  $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'ContactCtrl'
+  ,img: 'ices', ttl: 'Contact Smashers Near You'});
+  $routeProvider.when('/submit', {templateUrl: 'partials/submit.html', controller: 'DummyCtrl'
+  ,img: 'm2k', ttl: 'Submit a Tournament'});
   //can i get this to work but not actually change the URL?
-  $routeProvider.when('/tournaments/:tourneyId', {templateUrl: 'partials/tournament.html', controller: 'TourneyCtrl'});
-  $routeProvider.when('/profile', {templateUrl: 'partials/profile.html', controller: 'ProfileCtrl'});
-  $routeProvider.when('/profiles/:playerId', {templateUrl: 'partials/profile.html', controller: 'ProfileCtrl'});
+  $routeProvider.when('/tournaments/:tourneyId', {templateUrl: 'partials/tournament.html', controller: 'TourneyCtrl'
+  ,img: 'RMV', ttl: 'REMOVEMEREMOVEMEREMOVEME'});
+  $routeProvider.when('/profile', {templateUrl: 'partials/profile.html', controller: 'ProfileCtrl'
+  ,img: 'RMV', ttl: 'REMOVEMEREMOVEMERMEOVEME'});
+  $routeProvider.when('/profiles/:playerId', {templateUrl: 'partials/profile.html', controller: 'ProfileCtrl'
+  ,img: 'RMV', ttl: 'REMOVEMREMOVEMEREMOVEME'});
   $routeProvider.otherwise({redirectTo: '/rankings'});
+  //should make a 404 page, its really easy
 }]);
 
