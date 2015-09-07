ca0315344c56c8bec4f9ce6544879cdedee5b7d8
Sam Nelson <snelson3@cs.uoregon.edu>
2015-09-07 19:24:50 -0400

    removed most todo comments to be placed in one list

--- app/ui/js/app.js
+++ app/ui/js/app.js
@@ -9,7 +9,6 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/meleerankings',{templateUrl:'ui/partials/meleerankings.html',controller: 'meleeRankingController', img:'falcon',title:'Melee Rankings',rankings:true});
   $routeProvider.when('/tournaments', {templateUrl: 'ui/partials/tournaments.html', controller: 'tournamentsController', img:'mango', title:'Tournaments'});
   $routeProvider.when('/contact', {templateUrl: 'ui/partials/contact.html', controller: 'contactController', img:'icies', title:'Contact Us'});
-  //can i get this to work but not actually change the URL?
   $routeProvider.when('/tournaments/:tourneyId', {templateUrl: 'ui/partials/tournament.html', controller: 'tournamentResultsController',img:'mango', title:'Tournaments'});
   $routeProvider.when('/profiles/:playerId', {templateUrl: 'ui/partials/profile.html', controller: 'profileController'});
   $routeProvider.when('/rules',{templateUrl: 'ui/partials/rules.html',img:'foxo',title:'Ruleset'})
