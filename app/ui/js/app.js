1766ad272a9f14b0af3e131a1c3f157be6a1b265
snelson3 <snelson@opalytics.com>
2015-09-13 13:00:09 -0700

    renamed partials to templates

--- app/ui/js/app.js
+++ app/ui/js/app.js
@@ -5,13 +5,13 @@ var myApp = angular.module('myApp', [
   'filters',
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/pmrankings', {templateUrl: 'ui/partials/pmrankings.html', controller: 'pmRankingController', img:'pmbowser', title:'Project M Rankings', rankings:true});
-  $routeProvider.when('/meleerankings',{templateUrl:'ui/partials/meleerankings.html',controller: 'meleeRankingController', img:'falcon',title:'Melee Rankings',rankings:true});
-  $routeProvider.when('/tournaments', {templateUrl: 'ui/partials/tournaments.html', controller: 'tournamentsController', img:'mango', title:'Tournaments'});
-  $routeProvider.when('/contact', {templateUrl: 'ui/partials/contact.html', controller: 'contactController', img:'icies', title:'Contact Us'});
-  $routeProvider.when('/tournaments/:tourneyId', {templateUrl: 'ui/partials/tournament.html', controller: 'tournamentResultsController',img:'mango', title:'Tournaments'});
-  $routeProvider.when('/profiles/:playerId', {templateUrl: 'ui/partials/profile.html', controller: 'profileController'});
-  $routeProvider.when('/rules',{templateUrl: 'ui/partials/rules.html',img:'foxo',title:'Ruleset'})
+  $routeProvider.when('/pmrankings', {templateUrl: 'ui/templates/pmrankings.html', controller: 'pmRankingController', img:'pmbowser', title:'Project M Rankings', rankings:true});
+  $routeProvider.when('/meleerankings',{templateUrl:'ui/templates/meleerankings.html',controller: 'meleeRankingController', img:'falcon',title:'Melee Rankings',rankings:true});
+  $routeProvider.when('/tournaments', {templateUrl: 'ui/templates/tournaments.html', controller: 'tournamentsController', img:'mango', title:'Tournaments'});
+  $routeProvider.when('/contact', {templateUrl: 'ui/templates/contact.html', controller: 'contactController', img:'icies', title:'Contact Us'});
+  $routeProvider.when('/tournaments/:tourneyId', {templateUrl: 'ui/templates/tournament.html', controller: 'tournamentResultsController',img:'mango', title:'Tournaments'});
+  $routeProvider.when('/profiles/:playerId', {templateUrl: 'ui/templates/profile.html', controller: 'profileController'});
+  $routeProvider.when('/rules',{templateUrl: 'ui/templates/rules.html',img:'foxo',title:'Ruleset'})
   $routeProvider.otherwise({redirectTo: 'pmrankings'});
 }]);
 
