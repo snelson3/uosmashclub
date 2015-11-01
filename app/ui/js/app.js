4271af579657a250dcb7147b90039bad04633c20
Sam Nelson <snelson3@cs.uoregon.edu>
2015-11-01 17:35:35 -0500

    added new rankings homepage

--- app/ui/js/app.js
+++ app/ui/js/app.js
@@ -5,13 +5,14 @@ var uoSmash = angular.module('uoSmash', [
   'filters',
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/pmrankings', {templateUrl: 'ui/templates/pmrankings.html', controller: 'pmRankingController', img:'pmbowser', title:'Project M Rankings', rankings:true});
-  $routeProvider.when('/meleerankings',{templateUrl:'ui/templates/meleerankings.html',controller: 'meleeRankingController', img:'falcon',title:'Melee Rankings',rankings:true});
-  $routeProvider.when('/tournaments', {templateUrl: 'ui/templates/tournaments.html', controller: 'tournamentsController', img:'mango', title:'Tournaments'});
-  $routeProvider.when('/contact', {templateUrl: 'ui/templates/contact.html', controller: 'contactController', img:'icies', title:'Contact Us'});
-  $routeProvider.when('/tournaments/:tourneyId', {templateUrl: 'ui/templates/tournament.html', controller: 'tournamentResultsController',img:'mango', title:'Tournaments'});
+  $routeProvider.when('/pmrankings', {isJPG:true, templateUrl: 'ui/templates/pmrankings.html', controller: 'pmRankingController', img:'pmbowser', title:'Project M Rankings', rankings:true});
+  $routeProvider.when('/meleerankings',{isJPG:true, templateUrl:'ui/templates/meleerankings.html',controller: 'meleeRankingController', img:'falcon',title:'Melee Rankings',rankings:true});
+  $routeProvider.when('/tournaments', {isJPG:true, templateUrl: 'ui/templates/tournaments.html', controller: 'tournamentsController', img:'mango', title:'Tournaments'});
+  $routeProvider.when('/contact', {isJPG:true, templateUrl: 'ui/templates/contact.html', controller: 'contactController', img:'icies', title:'Contact Us'});
+  $routeProvider.when('/tournaments/:tourneyId', {isJPG:true, templateUrl: 'ui/templates/tournament.html', controller: 'tournamentResultsController',img:'mango', title:'Tournaments'});
   $routeProvider.when('/profiles/:playerId', {templateUrl: 'ui/templates/profile.html', controller: 'profileController'});
-  $routeProvider.when('/rules',{templateUrl: 'ui/templates/rules.html',img:'foxo',title:'Ruleset'})
-  $routeProvider.otherwise({redirectTo: 'pmrankings'});
+  $routeProvider.when('/rules',{isJPG:true, templateUrl: 'ui/templates/rules.html',img:'foxo',title:'Ruleset'});
+  $routeProvider.when('/rankings',{isPNG:true, templateUrl: 'ui/templates/rankings.html',img:'uosmash',title:'UO Smash Club Rankings'});
+  $routeProvider.otherwise({redirectTo: 'rankings'});
 }]);
 
