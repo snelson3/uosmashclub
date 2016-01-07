3e7f3a36362c9138a5e755ee2b7451f6ea9eacea
Sam Nelson <snelson3@cs.uoregon.edu>
2016-01-07 11:04:49 -0800

    got rankings displaying on rankings page

--- app/ui/js/app.js
+++ app/ui/js/app.js
@@ -5,14 +5,14 @@ var uoSmash = angular.module('uoSmash', [
   'filters',
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/pmrankings', {isJPG:true, templateUrl: 'ui/templates/rankings.tmpl.html', controller: 'rankingsController', img:'pmbowser', title:'Project M Rankings', rankings:true, game:"pm"});
-  $routeProvider.when('/meleerankings',{isJPG:true, templateUrl:'ui/templates/rankings.tmpl.html',controller: 'rankingsController', img:'falcon',title:'Melee Rankings',rankings:true, game:"melee"});
-  $routeProvider.when('/s4rankings',{templateUrl:'ui/templates/rankings.tmpl.html',controller:'rankingsController',title:'Smash 4 Rankings',rankings:true,game:'s4'});
+  $routeProvider.when('/pmrankings', {isPNG:true, templateUrl: 'ui/templates/rankings.tmpl.html', controller: 'rankingsController', img:'uosmash', title:'Project M Rankings', rankings:true, game:"pm"});
+  $routeProvider.when('/meleerankings',{isPNG:true, templateUrl:'ui/templates/rankings.tmpl.html',controller: 'rankingsController', img:'uosmash', title:'Melee Rankings',rankings:true, game:"melee"});
+  $routeProvider.when('/s4rankings',{isPNG:true, templateUrl:'ui/templates/rankings.tmpl.html',controller:'rankingsController', img:'uosmash', title:'Smash 4 Rankings',rankings:true,game:'s4'});
   $routeProvider.when('/tournaments', {isJPG:true, templateUrl: 'ui/templates/tournaments.html', controller: 'tournamentsController', img:'mango', title:'Tournaments'});
   $routeProvider.when('/contact', {isJPG:true, templateUrl: 'ui/templates/contact.html', controller: 'contactController', img:'icies', title:'Contact Us'});
   $routeProvider.when('/tournaments/:tourneyId', {isJPG:true, templateUrl: 'ui/templates/tournament.html', controller: 'tournamentResultsController',img:'mango', title:'Tournaments'});
   $routeProvider.when('/profiles/:playerId', {templateUrl: 'ui/templates/profile.html', controller: 'profileController'});
   $routeProvider.when('/rules',{isJPG:true, templateUrl: 'ui/templates/rules.html',img:'foxo',title:'Ruleset'});
-  $routeProvider.when('/rankings',{isPNG:true, templateUrl: 'ui/templates/rankings.html',img:'uosmash',title:'UO Smash Club Rankings'});
+  $routeProvider.when('/rankings',{isPNG:true, templateUrl: 'ui/templates/rankings.html', controller: 'homeController', img:'uosmash',title:'UO Smash Club Rankings'});
   $routeProvider.otherwise({redirectTo: 'rankings'});
 }]);
---
+++ app/ui/js/controllers/home.controller.js
@@ -0,0 +1,43 @@
+uoSmash.controller('homeController', ['$scope', '$http', 'rankingService', 'profileService',
+  function($scope, $http,rankingService,profileService) {
+    $scope.meleeprofiles=[];
+    $scope.pmprofiles=[];
+    $scope.s4profiles=[];
+
+    var player = "";
+
+
+    $scope.getProfile = function(pid,profiles) {
+      profileService.getProfile(pid).success(function(data) {
+        profiles[profiles.length] = data;
+    });};
+
+    $scope.getPlayers = function() {
+      rankingService.getPlayerList('melee').success(function(data) {
+        $scope.players = data;
+        for (player in $scope.players) {
+              $scope.getProfile($scope.players[player].name,$scope.meleeprofiles);
+          }
+       });
+
+       rankingService.getPlayerList('pm').success(function(data) {
+         $scope.players = data;
+         for (player in $scope.players) {
+               $scope.getProfile($scope.players[player].name,$scope.pmprofiles);
+           }
+
+        });
+
+        rankingService.getPlayerList('s4').success(function(data) {
+          $scope.players = data;
+          for (player in $scope.players) {
+                $scope.getProfile($scope.players[player].name,$scope.s4profiles);
+            }
+         });
+     };
+
+    $scope.getPlayers();
+
+    $scope.orderProp="-rating";
+  }
+]);
