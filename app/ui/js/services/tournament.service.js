300b6dbbe80e1de6bdf921ff58b633b0ecab4ae9
Sam Nelson <snelson3@cs.uoregon.edu>
2016-01-07 11:30:22 -0800

    added support for .uotn files

--- app/ui/js/services/tournament.service.js
+++ app/ui/js/services/tournament.service.js
@@ -1,14 +1,14 @@
 uoSmash.factory('tournamentService', [ '$http', function($http) {
 	var _getTournamentResults = function(tid) {
-		return $http.get('data/tournaments/'+tid+'.json');
+		return $http.get('data/tournaments/'+tid+'.uotn');
 	};
 
 	var _getUpcomingTournaments = function() {
-		return $http.get('data/upcomingtournaments.json');
+		return $http.get('data/upcomingtournaments.uotn');
 	};
 
 	var _getPastTournaments = function() {
-		return $http.get('data/pasttournaments.json');
+		return $http.get('data/pasttournaments.uotn');
 	};
 
 	return {
