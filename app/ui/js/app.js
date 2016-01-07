1388c78ecb43816bb0a80a6a85b73f2409dec67e
Sam Nelson <snelson3@cs.uoregon.edu>
2016-01-06 18:18:16 -0800

    added smash4 page

--- app/ui/js/app.js
+++ app/ui/js/app.js
@@ -7,6 +7,7 @@ var uoSmash = angular.module('uoSmash', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/pmrankings', {isJPG:true, templateUrl: 'ui/templates/rankings.tmpl.html', controller: 'rankingsController', img:'pmbowser', title:'Project M Rankings', rankings:true, game:"pm"});
   $routeProvider.when('/meleerankings',{isJPG:true, templateUrl:'ui/templates/rankings.tmpl.html',controller: 'rankingsController', img:'falcon',title:'Melee Rankings',rankings:true, game:"melee"});
+  $routeProvider.when('/s4rankings',{templateUrl:'ui/templates/rankings.tmpl.html',controller:'rankingsController',title:'Smash 4 Rankings',rankings:true,game:'s4'});
   $routeProvider.when('/tournaments', {isJPG:true, templateUrl: 'ui/templates/tournaments.html', controller: 'tournamentsController', img:'mango', title:'Tournaments'});
   $routeProvider.when('/contact', {isJPG:true, templateUrl: 'ui/templates/contact.html', controller: 'contactController', img:'icies', title:'Contact Us'});
   $routeProvider.when('/tournaments/:tourneyId', {isJPG:true, templateUrl: 'ui/templates/tournament.html', controller: 'tournamentResultsController',img:'mango', title:'Tournaments'});
